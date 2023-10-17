7384c1ac01637a0bec16da083fde5a8e28ea4b64
José Daniel Mantilla Franqui <danielfranqui016@gmail.com>
2023-10-17 08:33:41 -0500

    Añadida funcionalidad para indicar al usuario si el mensaje no fue enviado

--- server.js
+++ server.js
@@ -33,8 +33,7 @@ app.post('/enviar', (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
-      res.send('Hubo un error al enviar el mensaje.');
-      alert("No se pudo enviar el mensaje :( .")
+      res.redirect('/denegacion.html');
     } else {
       console.log('Correo electrónico enviado: ' + info.response);
       res.redirect('/confirmacion.html');
